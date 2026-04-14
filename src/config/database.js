d9633c59eb3c354bf064fdb5833b61bf3c5899a7
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-04-14 17:45:07 +0530

    updated config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,31 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constant.js";
 
-const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      process.env.MONGODB_URI,
-      {
-        dbName: DB_NAME
-      }
-    );
+const MONGODB_URI = process.env.MONGODB_URI;
 
-    console.log(`✅ MongoDB Connected!!! ${connectionInstance.connection.host}`);
-    
-  } catch (error) {
-    console.error("❌ Connection Failed!!!", error.message);
-    process.exit(1);
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is missing in environment variables");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: DB_NAME
+    }).then((mongoose) => {
+      return mongoose;
+    });
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
