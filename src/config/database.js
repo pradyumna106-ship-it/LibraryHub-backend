30db8018e2b6405bc1d8107b804320b1b2c5fedc
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-04-15 17:47:21 +0530

    added consol.error to find exact issue

--- src/config/database.js
+++ src/config/database.js
@@ -5,14 +5,16 @@ let isConnected = false;
 export const connectDB = async () => {
   if (isConnected) return;
 
-  // if (!process.env.MONGODB_URI) {
-  //   throw new Error("MONGODB_URI is missing");
-  // }
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://pradyumnajekumar_db_user:OOUAMTsOKJamEdD1@cluster0.5xvzjta.mongodb.net/library",
+      { bufferCommands: false }
+    );
 
-  const conn = await mongoose.connect("mongodb+srv://pradyumnajekumar_db_user:OOUAMTsOKJamEdD1@cluster0.5xvzjta.mongodb.net/library", {
-    bufferCommands: false,
-  });
-
-  isConnected = conn.connections[0].readyState === 1;
-  console.log("MongoDB connected");
+    isConnected = conn.connections[0].readyState === 1;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("DB ERROR:", err);
+    throw err;
+  }
 };
