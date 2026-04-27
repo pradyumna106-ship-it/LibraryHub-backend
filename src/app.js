7228b5c04812be265de6e41da75141f187f1d122
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-04-27 13:21:25 +0530

    removed undefined methods

--- src/app.js
+++ src/app.js
@@ -18,9 +18,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
   credentials: true}));
-app.options('/{*path}', cors());
-app.use('/{*path}', handler);
-app.get('/{*path}', handler);
+app.options('/api', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form
 app.use("/uploads", express.static("uploads"));
